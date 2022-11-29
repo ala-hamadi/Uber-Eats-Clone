6118e2ec81aea2544b5d46bf813635e90d781149
ala-hamadi <ala.hamadi.01@gmail.com>
2022-11-29 15:33:45 +0100

    Add restaurant detail

---
+++ components/restaurantDetail/About.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+
+const image =
+  "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg";
+
+const title = "Farmhouse Kitchen Thai Cuisine";
+const description = "Thai • Comfort Foot • $$";
+export default function About() {
+  return (
+    <View>
+      <RestaurantImage image={image} />
+      <RestaurantName name={title} />
+      <RestaurantDescription description={description} />
+    </View>
+  );
+}
+
+const RestaurantImage = (props) => (
+  <Image source={{ uri: props.image }} style={{ width: "100%", height: 180 }} />
+);
+
+const RestaurantName = (props) => (
+  <Text
+    style={{
+      fontSize: 29,
+      fontWeight: "600",
+      marginTop: 10,
+      marginHorizontal: 15,
+    }}
+  >
+    {props.name}
+  </Text>
+);
+
+const RestaurantDescription = (props) => (
+  <Text
+    style={{
+      marginTop: 10,
+      marginHorizontal: 15,
+      fontWeight: "400",
+      fontSize: 15.5,
+    }}
+  >
+    {props.description}
+  </Text>
+);
