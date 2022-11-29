5045a0dbaa78ab400b14766f246a880848006f3e
ala-hamadi <ala.hamadi.01@gmail.com>
2022-11-29 14:17:21 +0100

    Bottom Tabs

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { View, StatusBar, SafeAreaView, ScrollView } from "react-native";
+import { Divider } from "react-native-elements/dist/divider/Divider";
+import BottomTabs from "../components/BottomTabs";
 import Categories from "../components/Categories";
 
 import HeaderTabs from "../components/HeaderTabs";
@@ -23,6 +25,10 @@ export default function Home() {
         <Categories />
         <RestaurantItems />
       </ScrollView>
+      <View style={{ backgroundColor: "white" }}>
+        <Divider width={1} />
+        <BottomTabs />
+      </View>
     </SafeAreaView>
   );
 }
